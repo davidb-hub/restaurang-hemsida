51d2fcf255cf3988c3007f0e0452f9a68d90f414
Zsombor Simon <zsombor.simonguth@gmail.com>
2021-02-18 14:20:49 +0100

    Included David's changes

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("animation-stop");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("animation-stop");
+  }, 400);
+});
+
+
+const burgerSlider = () => {
+
+    const burger = document.querySelector('.burger-nav');
+    const nav = document.querySelector('.nav-links');
+    const textBox = document.querySelector('.content-container');
+
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('burger-active');
+        textBox.classList.toggle('fade-textBox');
+
+        burger.classList.toggle('burger-active');
+
+    });
+
+}
+
+burgerSlider();
